036816b94f8349e4598abbf50f4ee0441f70e77f
Edsuns <edsuns@qq.com>
2021-01-07 10:18:28 +0800

    Add Checksum.kt - Update HashUtilities.kt;

--- ad-filter/src/main/java/io/github/edsuns/adfilter/HashUtilities.kt
+++ ad-filter/src/main/java/io/github/edsuns/adfilter/HashUtilities.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (c) 2018 DuckDuckGo
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.github.edsuns.adfilter
 
 import java.math.BigInteger
@@ -30,12 +14,20 @@ fun ByteArray.verifySha256(sha256: String): Boolean {
 val String.sha1: String
     get() = sha("SHA-1", this.toByteArray())
 
+fun String.verifySha1(sha1: String): Boolean {
+    return this.sha1 == sha1
+}
+
 private fun sha(algorithm: String, bytes: ByteArray): String {
-    val md = MessageDigest.getInstance(algorithm)
-    val digest = md.digest(bytes)
+    val digest = hash(algorithm, bytes)
     return String.format("%0" + digest.size * 2 + "x", BigInteger(1, digest))
 }
 
-fun String.verifySha1(sha1: String): Boolean {
-    return this.sha1 == sha1
+fun md5(bytes: ByteArray): ByteArray {
+    return hash("MD5", bytes)
+}
+
+private fun hash(algorithm: String, bytes: ByteArray): ByteArray {
+    val md = MessageDigest.getInstance(algorithm)
+    return md.digest(bytes)
 }
